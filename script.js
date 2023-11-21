30dddff58abec061f3fd3a95fb671211d1dc0e18
John <johndoe@example.com>
2023-11-21 18:37:44 +0100

    config_added

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
-const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
-const litePhrases = ['Pas de panique !', 'La vie, l’univers et le reste', 'Merci pour le poisson']
 let score = 0
 
 let choix = prompt("Veuillez choisir la liste: mots ou phrases")
-while(choix !== "mots" && choix !== "phrases"){
+while (choix !== "mots" && choix !== "phrases") {
     choix = prompt("Veuillez choisir la liste: mots ou phrases")
 }
 
-if (choix === "mots"){
+if (choix === "mots") {
     for (let i = 0; i < listeMots.length; i++) {
         // Déclaration de la variable contenant le mot saisi par l'utilisateur
         // Le prompt sert à afficher une popup demandant à l'utilisateur de saisir un mot
         let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
-    
+
         // Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
         if (motUtilisateur === listeMots[i]) {
             score++
@@ -23,12 +20,12 @@ if (choix === "mots"){
     console.log("Votre score est de " + score + " sur " + listeMots.length)
 } else {
     for (let i = 0; i < litePhrases.length; i++) {
-        let motUtilisateur = prompt('Entrez le mot : ' + litePhrases[i])
+        let motUtilisateur = prompt('Entrez la phrse : ' + litePhrases[i])
         if (motUtilisateur === litePhrases[i]) {
             score++
         }
     }
-    
+
     // Affichage du score de l'utilisateur
     console.log("Votre score est de " + score + " sur " + litePhrases.length)
 }
